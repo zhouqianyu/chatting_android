2ac3c0f55fd26cd6cfb3c8a9fe7eebf3457a5582
shaoxin <shao-xin@qq.com>
2019-05-02 09:12:06 +0800

    添加了一些依赖，将主题配置到qmui的主题，添加了okhttp，活动池，lru缓存这些常用的工具类

---
+++ app/src/main/java/com/bysj/chatting/util/Constant.java
@@ -0,0 +1,35 @@
+package com.bysj.chatting.util;
+
+/**
+ * Created by shaoxin on 18-3-25.
+ * 这是一个存放常量的类
+ */
+
+public class Constant {
+
+    // 这个是服务器的IP地址
+    public static final String SERVER_IP = "https://www.fstechnology.cn";
+
+    // 本地IP地址
+//    public static final String SERVER_IP = "http://192.168.199.173";
+
+    // 设置服务器端口
+    public static final String SERVER_PORT = "8443";
+
+    // 基础数据请求地址
+    public static final String BASE_DB_URL = SERVER_IP + ":" + SERVER_PORT + "/YZEduDBServer/";
+
+    // 基础图片请求路径
+    public static final String BASE_IMG_URL = "http://cdn.fstechnology.cn/YZEduResources/images/";
+
+    // 基础文件请求路径
+    public static final String BASE_FILE_URL = "http://cdn.fstechnology.cn/YZEduResources/other/";
+
+    // 基础视频请求路径
+    public static final String BASE_VIDEO_URL = "http://cdn.fstechnology.cn/YZEduResources/videos/";
+
+
+
+
+
+}
