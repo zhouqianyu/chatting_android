0ed7e6b9b491bb6fe10fbabdbec6977b04c49883
shaoxin <shao-xin@qq.com>
2019-05-04 13:06:15 +0800

    将字体单位改成sp

--- app/src/main/java/com/bysj/chatting/activity/ChattingActivity.java
+++ app/src/main/java/com/bysj/chatting/activity/ChattingActivity.java
@@ -1,9 +1,18 @@
 package com.bysj.chatting.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by shaoxin on 19-5-2.
  * 聊天页
  */
 
-public class ChattingActivity {
+public class ChattingActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }
