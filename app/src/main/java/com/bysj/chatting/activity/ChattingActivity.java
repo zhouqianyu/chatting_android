73082c3478d1a80fb4207fae0f850c42539e6efa
shaoxin <shao-xin@qq.com>
2019-05-19 11:51:03 +0800

    完成语音，存档

--- app/src/main/java/com/bysj/chatting/activity/ChattingActivity.java
+++ app/src/main/java/com/bysj/chatting/activity/ChattingActivity.java
@@ -1,14 +1,20 @@
 package com.bysj.chatting.activity;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
@@ -24,6 +30,8 @@ import com.bysj.chatting.application.ChattingApplication;
 import com.bysj.chatting.bean.ChattingBean;
 import com.bysj.chatting.broadcast.MyReceiver;
 import com.bysj.chatting.service.MqttService;
+import com.bysj.chatting.util.AudioFileFunc;
+import com.bysj.chatting.util.AudioRecordFunc;
 import com.bysj.chatting.util.CallBackUtil;
 import com.bysj.chatting.util.Constant;
 import com.bysj.chatting.util.OkhttpUtil;
@@ -36,6 +44,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -64,6 +73,7 @@ public class ChattingActivity extends AppCompatActivity {
 
     private EditText etInput;
     private QMUIRoundButton bnSend;
+    private QMUIRoundButton bnRecord;
 
     private List<ChattingBean> listItems;
     private ChattingAdapter adapter;
@@ -71,6 +81,9 @@ public class ChattingActivity extends AppCompatActivity {
     // 广播
     MyReceiver receiver;
     IntentFilter filter;
+
+    private int MY_PERMISSIONS_REQUEST_CALL_PHONE;
+    private AudioRecordFunc func;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -95,9 +108,11 @@ public class ChattingActivity extends AppCompatActivity {
         lvChatting = findViewById(R.id.lv_chatting);
         etInput = findViewById(R.id.et_input);
         bnSend = findViewById(R.id.bn_send);
+        bnRecord = findViewById(R.id.bn_record);
         listItems = new ArrayList<>();
         adapter = new ChattingAdapter(ChattingActivity.this, listItems);
         lvChatting.setAdapter(adapter);
+        func = AudioRecordFunc.getInstance();
 
         bnSend.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -165,6 +180,7 @@ public class ChattingActivity extends AppCompatActivity {
                 llTextGroup.setVisibility(View.VISIBLE);
             }
         });
+
     }
 
     /**
@@ -220,8 +236,6 @@ public class ChattingActivity extends AppCompatActivity {
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
-                } finally {
-
                 }
             }
         });
@@ -298,4 +312,124 @@ public class ChattingActivity extends AppCompatActivity {
         }
     }
 
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        //按下操作
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            startRecord();
+        }
+        //抬起操作
+        if (motionEvent.getAction() == MotionEvent.ACTION_UP || motionEvent.getAction() == MotionEvent.ACTION_CANCEL) {
+            stopRecord();
+        }
+        //移动操作
+        if (motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
+
+        }
+        return super.onTouchEvent(motionEvent);
+    }
+
+    /**
+     * 按下
+     */
+    private void startRecord() {
+        Log.e("event", "start");
+        bnRecord.setText("我在听...");
+        if (ContextCompat.checkSelfPermission(ChattingActivity.this,
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(ChattingActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(ChattingActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(ChattingActivity.this,
+                    new String[]{Manifest.permission.RECORD_AUDIO,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_CALL_PHONE);
+        } else {
+            int i = func.startRecordAndFile();
+            Log.e("is", i + "");
+        }
+    }
+
+    /**
+     * 松开
+     */
+    private void stopRecord() {
+        Log.e("event", "end");
+        bnRecord.setText("识别中...");
+        func.stopRecordAndFile();
+        String path = AudioFileFunc.getWavFilePath();
+        long size = func.getRecordFileSize();
+        if (size > 70000) {
+            upload(path);
+        } else {
+            bnRecord.setText("按住 说话");
+            Toast.makeText(ChattingActivity.this, "录音时间过短", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_CALL_PHONE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(ChattingActivity.this, "recoarding", Toast.LENGTH_SHORT).show();
+                int i = func.startRecordAndFile();
+                Log.e("is", i + "");
+            } else {
+                // Permission Denied
+                Toast.makeText(ChattingActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    /**
+     * 上传文件
+     */
+    private void upload(String path) {
+        File file = new File(path);
+        String url = Constant.BASE_DB_URL + "message/recognize";
+        Map<String, String> map = new HashMap<>();
+        map.put("token", application.getToken());
+        OkhttpUtil.okHttpUploadFile(url, file, "file", "file", map, new CallBackUtil.CallBackString() {
+            @Override
+            public void onFailure(Call call, Exception e) {
+                Toast.makeText(ChattingActivity.this, R.string.server_response_error, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(String response) {
+                Log.e("res", response);
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    int code = jsonObject.getInt("code");
+                    if (code == 200) {
+                        String data = jsonObject.getString("data");
+                        recognizeSuccess(data);
+                    } else {
+                        String msg = jsonObject.getString("msg");
+                        Toast.makeText(ChattingActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.e("err", e.getLocalizedMessage());
+                } finally {
+                    bnRecord.setText("按住 说话");
+                }
+            }
+        });
+    }
+
+    private void recognizeSuccess(String str) {
+        llAudioGroup.setVisibility(View.GONE);
+        llTextGroup.setVisibility(View.VISIBLE);
+        etInput.setText("");
+        etInput.setText(str);
+    }
 }
